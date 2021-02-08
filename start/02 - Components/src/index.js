67ce4d03284098e55e124ce58f9e424eb2d5f6ef
Lucas Denduyver <Lucas.denduyver@student.howest.be>
2021-02-08 11:31:48 +0100

    deel1

---
+++ start/02 - Components/src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+const GetHello = () => {
+  const naam = "Lucas";
+  return (
+    <>
+    <p>Ik <span className="name">ben </span>{naam}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const showDislikes = false;
+  const favFood = [
+    "pasta bolognese",
+    "pizza",
+    "chinees"
+  ];
+  const badFood = [
+    "spruiten sws",
+    "schorseneren",
+    "rode kool",
+    "risotto",
+    "koolsla",
+    "te droog vlees",
+    "ebde nog efkes?"
+  ];
+  return (
+  <>
+  {GetHello()}
+
+  {showDislikes ?
+    <ul>
+      {favFood.map(item =><li key={item}> {item} </li>)}
+    </ul>
+    :
+    <ul>
+      {badFood.map(item =><li key={item}> {item} </li>)}
+  </ul>
+  }
+  </>);
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GetHello/>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
