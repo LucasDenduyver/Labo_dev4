8b84b4fcbd6c00e0c7113f6c34931ec8184e86a4
Lucas Denduyver <Lucas.denduyver@student.howest.be>
2021-02-08 10:02:39 +0100

    master push

---
+++ start/01 - JSX/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const App = () => {
+  return <p>Hello</p>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
