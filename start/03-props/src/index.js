def9dd773b955a3acf63b8e6fcbe9943689a2bd8
Lucas Denduyver <Lucas.denduyver@student.howest.be>
2021-02-08 12:14:12 +0100

    enkele errors, ga herbekijken met oplossingen

---
+++ start/03-props/src/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const FoodList =  (props) => {
+  return (
+    <>
+      <ul>
+        {props.list.map(item =><li key={item}> {item} </li>)}
+      </ul>
+    </>);
+}
+
+const GetHello = () => {
+  const naam = "Lucas";
+  return (
+    <>
+    <p>Ik <span className="name">ben </span>{naam}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const showDislikes = false;
+  const favFood = [
+    "pasta bolognese",
+    "pizza",
+    "chinees"
+  ];
+  const badFood = [
+    "spruiten sws",
+    "schorseneren",
+    "rode kool",
+    "risotto",
+    "koolsla",
+    "te droog vlees",
+    "ebde nog efkes?"
+  ];
+  return (
+  <>
+
+  {showDislikes ?
+    <ul>
+      {favFood.map(item =><li key={item}> {item} </li>)}
+    </ul>
+    :
+    <ul>
+      {badFood.map(item =><li key={item}> {item} </li>)}
+  </ul>
+  }
+  </>);
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FoodList list={badFood} />
+    <GetHello />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
